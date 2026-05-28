5be8732b17fad27000b441e988e5591746de17a3
plexlevi <plexlevi@gmail.com>
2026-05-28 06:24:02 +0200

    chore(release): v0.4.13

--- app/shared/version.py
+++ app/shared/version.py
@@ -3,7 +3,7 @@
 """Application version metadata."""
 
 APP_NAME = "FFBuilder"
-APP_VERSION = "0.4.12"
+APP_VERSION = "0.4.13"
 GITHUB_REPO = "plexlevi/FFBuilder"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPO}/releases"
 
